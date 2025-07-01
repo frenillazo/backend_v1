ecc45962bfa58ebe5ee9f708a3251ad4a75a4339
PabloCalahorro <pablocalahorrojimenez@gmail.com>
2025-07-01 12:02:30 +0200

    Initial commit

---
+++ src/test/java/com/acainfo/backend_v1/BackendV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acainfo.backend_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
